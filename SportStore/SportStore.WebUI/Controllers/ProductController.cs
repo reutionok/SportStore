0490ddf4fe5d1c6187b4598b1e62b842c7d3b242
reutionok <reutionok@users.noreply.github.com>
2020-07-20 16:10:19 +0300

    new mvc project. first preparations. add ninject

---
+++ SportStore/SportStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using SportStore.Domain.Abstract;
+using SportStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
---
+++ SportStore/SportStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using SportStore.Domain.Entities;
+using SportStore.Domain.Abstract;
+
+namespace SportStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+                mock.Setup(m => m.Products).Returns(new List<Product>
+        {
+            new Product { Name = "SimCity", Price = 1499 },
+            new Product { Name = "TITANFALL", Price=2299 },
+            new Product { Name = "Battlefield 4", Price=899.4M }
+        });
+                kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            }
+
+        }
+    }
