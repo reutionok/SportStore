b9869702880151f6991caf7738f76246392b7287
reutionok <reutionok@users.noreply.github.com>
2020-07-20 19:40:10 +0300

    add pagination

--- SportStore/SportStore.WebUI/Controllers/ProductController.cs
+++ SportStore/SportStore.WebUI/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using SportStore.Domain.Abstract;
 using SportStore.Domain.Entities;
+using SportStore.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +12,27 @@ namespace SportStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int pageSize = 4;
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+           ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                       .OrderBy(product => product.ProductID)
+                       .Skip((page - 1) * pageSize)
+                       .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
---
+++ SportStore/SportStore.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SportStore.Domain.Entities;
+using System.Collections.Generic;
+
+
+namespace SportStore.WebUI.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
